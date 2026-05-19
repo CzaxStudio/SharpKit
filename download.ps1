04a7436378a2f8c03f76539e0a10878d7c180c96
Hacker <minubgp@gmail.com>
2026-05-19 10:51:53 +0530

    Create download.ps1

---
+++ download.ps1
@@ -0,0 +1,8 @@
+# Download using .NET CLI
+dotnet add package SharpKit.Offensive --version 1.0.5
+# Download using PMC
+NuGet\Install-Package SharpKit.Offensive -Version 1.0.5
+# Download using Packet CLI
+paket add SharpKit.Offensive --version 1.0.5
+# run using ./download.ps1
+
