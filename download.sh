dee84c3b0684b3baf50aada3f03e049aa1315d5c
Hacker <minubgp@gmail.com>
2026-05-20 12:25:36 +0530

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Download using .NET CLI
+dotnet add package SharpKit.Offensive --version 1.0.0
+dotnet add package SharpKit.Offensive --version 1.0.5
+dotnet add package SharpKit.Offensive --version 1.1.0
+
+# Download using NuGet CLI
+nuget install SharpKit.Offensive -Version 1.0.0
+nuget install SharpKit.Offensive -Version 1.0.5
+nuget install SharpKit.Offensive -Version 1.1.0
+
+# Download using Paket CLI
+paket add SharpKit.Offensive --version 1.0.0
+paket add SharpKit.Offensive --version 1.0.5
+paket add SharpKit.Offensive --version 1.1.0
+
+# Notes:
+
+# Save as download.sh
+# Make it executable: chmod +x download.sh
+# Run using: ./download.sh
+# With Love: CzaxStudio
